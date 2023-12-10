27f5c38a8bf8190385a19190dfb689235140fbd0
133usr <78338503+133usr@users.noreply.github.com>
2023-12-10 21:02:10 +0530

    init

---
+++ test/functional/source_file_gpx_3d.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+describe('source_file_gpx_3d', function _() {
+    let result;
+    before(async () => {
+        result = await loadExample('examples/source_file_gpx_3d.html', this.fullTitle());
+    });
+
+    it('view initialized', async () => {
+        assert.ok(result);
+    });
+
+    it('should wait for the mesh to be added to the scene', async function _it() {
+        await page.waitForFunction(() => view.scene.children.length === 5, { timeout: 10000 });
+    });
+});
